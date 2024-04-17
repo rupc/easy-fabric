b9071be9c1a5b44d13d8693c9190d71f45a9efb3
rupc <neurosilver@gmail.com>
2024-04-18 03:44:37 +0900

    Fixed orderer certificate error

--- cryptogen/generate.py
+++ cryptogen/generate.py
@@ -20,31 +20,45 @@ def generate_yaml_files(num_orgs, num_orderers):
 """
         peer_orgs.append(peer_org)
 
-    # Generate Orderer Organizations
-    for i in range(1, num_orderers + 1):
-        orderer_org = f"""
-  - Name: Orderer{i}
-    Domain: example.com
-    EnableNodeOUs: true
-    Specs:
-      - Hostname: orderer{i}
-        SANS:
-          - localhost
-"""
-        orderer_orgs.append(orderer_org)
-
     # Write to Peer Organizations YAML file
     peer_yaml_content = "PeerOrgs:\n" + "".join(peer_orgs)
     peer_file_name = f"crypto-config-{num_orgs}-org.yaml"
     with open(peer_file_name, 'w') as file:
         file.write(peer_yaml_content)
     print(f"YAML file '{peer_file_name}' has been created for peer organizations.")
+        # Generate Orderer Organizations
     
+    orderer_specs = "".join([f"""
+      - Hostname: orderer{i}
+        SANS:
+          - localhost""" for i in range(1, num_orderers + 1)
+    ])
+    
+    orderer_org = f"""
+  - Name: Orderer
+    Domain: example.com
+    EnableNodeOUs: true
+    Specs:
+{orderer_specs}
+"""
+    yaml_content = f"""
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+---
+OrdererOrgs:
+{orderer_org}
+"""
+    print(orderer_org)
     # Write to Orderer Organizations YAML file
-    orderer_yaml_content = "OrdererOrgs:\n" + "".join(orderer_orgs)
+    # orderer_yaml_content = "OrdererOrgs:\n" + "".join(orderer_orgs)
     orderer_file_name = f"crypto-config-{num_orderers}-orderer.yaml"
     with open(orderer_file_name, 'w') as file:
-        file.write(orderer_yaml_content)
+        file.write(yaml_content)
+
+    # with open(orderer_file_name, 'w') as file:
+    #     file.write(orderer_yaml_content)
     print(f"YAML file '{orderer_file_name}' has been created for orderer organizations.")
 
 def main():
