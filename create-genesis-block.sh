0a0dab05e58d2c936c5ff425b1e9da27b983148d
rupc <neurosilver@gmail.com>
2024-04-18 01:49:22 +0900

    Automatically generate genesis blocks for preconfigured profiles

--- create-genesis-block.sh
+++ create-genesis-block.sh
@@ -10,33 +10,19 @@ if [ ! -d "./channel-artifacts" ]; then
 fi
 
 organizations=(
-    "cryptogen/organizations-24org-6orderer"
-    "cryptogen/organizations-36org-9orderer"
-    "cryptogen/organizations-48org-12orderer"
-    "cryptogen/organizations-60org-15orderer"
+    "12orgs-3orderers"
+    "24orgs-6orderers"
+    "36orgs-9orderers"
+    "48orgs-12orderers"
+    "60orgs-15orderers"
 )
 
 for org_path in "${organizations[@]}"; do
     echo "Processing $org_path..."
-    # 여기에 각 경로에 대해 실행할 명령을 삽입합니다.
-    # 예: config 파일 생성 또는 파일 복사 등
-done
+    rm -rf ./organizations
+    cp cryptogen/organizations-$org_path ./organizations -rf
 
-# Loop through all yaml files in the configtx directory
-for CONFIGPATH in ./configtx/configtx-*; do
+    CONFIGPATH="./configtx/configtx-${org_path}"
 
-    # Extract the filename without path and extension
-    BASENAME=$(basename -- "$CONFIGPATH")
-    FILENAME="${BASENAME%.*}"
-
-    # Generate the genesis block using configtxgen
-    echo "Processing $CONFIGPATH..."
-    $BINPATH/configtxgen -profile ChannelUsingRaft -configPath "$CONFIGPATH" -outputBlock "./channel-artifacts/${FILENAME}.genesis.block" -channelID mychannel
-
-    # Check if the command was successful
-    if [ $? -eq 0 ]; then
-        echo "Genesis block generated for $FILENAME"
-    else
-        echo "Failed to generate genesis block for $FILENAME"
-    fi
+    $BINPATH/configtxgen -profile ChannelUsingRaft -configPath "$CONFIGPATH" -outputBlock "./channel-artifacts/${org_path}.genesis.block" -channelID mychannel
 done
